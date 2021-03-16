7d9ad016e359eb87b5de995f80ef3408346c22d8
Madhurima Nath <31106009+mnathvt@users.noreply.github.com>
2021-03-16 15:43:23 -0400

    Add files via upload

---
+++ model_comparison/net_gen.R
@@ -0,0 +1,13 @@
+#sch is the actual network given
+
+#saving the network as a edge list file
+a = as.matrix.network(sch, matrix.type = 'edgelist')
+write.table(a, file = '../Dropbox/AddHealth_Data/folder/school.txt', 
+            row.names = FALSE, col.names = FALSE)
+
+# this FMH network is a representative network of the actual data available in the literature. Here I extract it and save it as an edge list file
+
+fmh = faux.magnolia.high
+fmhn = as.matrix.network(fmh, matrix.type = 'edgelist')
+write.table(fmhn, file = '../Dropbox/AddHealth_Data/folder/fmh.txt', 
+            row.names = FALSE, col.names = FALSE)
